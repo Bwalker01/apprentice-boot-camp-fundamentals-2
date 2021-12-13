3656cb655b1709849201f6d6d6f98d7f3198f339
Ben <benjaminjwalker2910@gmail.com>
2021-12-13 11:21:26 +0000

    bowling tests kata first tests

---
+++ exercises/java/src/main/java/bowling/Bowling.java
@@ -0,0 +1,9 @@
+package bowling;
+
+public class Bowling {
+
+    public int calcScore(int[] rolls) {
+        return 0;
+    }
+
+}
--- exercises/java/src/test/java/bowling/BowlingTest.java
+++ exercises/java/src/test/java/bowling/BowlingTest.java
@@ -2,9 +2,12 @@ package bowling;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class BowlingTest {
     @Test
-    public void renameMe() {
-        
+    public void checkingEmptyScore() {
+        int[] rolls = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        assertThat(new Bowling().calcScore(rolls)).isEqualTo(0);
     }
 }
