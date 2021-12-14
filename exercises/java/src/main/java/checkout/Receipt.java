6988a65c9cd6ddda76b7c60bb8bfdff25037e7a4
Ben <benjaminjwalker2910@gmail.com>
2021-12-14 15:12:34 +0000

    new offer on "A"

--- exercises/java/src/main/java/checkout/Receipt.java
+++ exercises/java/src/main/java/checkout/Receipt.java
@@ -13,11 +13,10 @@ class Receipt {
 
     void scannedA() {
         text += "A: 50";
-        if (++numberOfA % 3 == 0) {
-            text += " - 20 (3 for 130)";
+        if (++numberOfA % 5 == 0) {
+            text += " - 30 (5 for 220)";
             total += 30;
-        }
-        else {
+        } else {
             total += 50;
         }
         text += "\n";
@@ -28,8 +27,7 @@ class Receipt {
         if (++numberOfB % 2 == 0) {
             text += " - 15 (2 for 45)";
             total += 15;
-        }
-        else {
+        } else {
             total += 30;
         }
         text += "\n";
--- exercises/java/src/test/java/checkout/ReceiptTests.java
+++ exercises/java/src/test/java/checkout/ReceiptTests.java
@@ -14,7 +14,7 @@ public class ReceiptTests {
                 "A: 50\n",
                 "Total: 50");
     }
-    
+
     @Test
     public void oneOfEach() {
         Checkout checkout = new Checkout();
@@ -44,10 +44,10 @@ public class ReceiptTests {
                 "A: 50\n",
                 "A: 50\n",
                 "B: 30\n",
-                "A: 50 - 20 (3 for 130)\n",
+                "A: 50\n",
                 "C: 20\n",
                 "D: 15\n",
                 "B: 30 - 15 (2 for 45)\n",
-                "Total: 210");
+                "Total: 230");
     }
 }
